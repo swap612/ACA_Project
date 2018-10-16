833c18ea01fd45ae9d891a5687ea8634fb2cfd42
Swapnil Raykar <swap612@gmail.com>
2018-10-16 11:36:32 +0530

    Updated the code.
    Solve all errors.
    added the header files.
    Updated the var names

---
+++ LLC.h
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+#define L3_SETS 2048
+#define L3_WAYS 8
+
+class LLC
+{
+	public:
+     unsigned long long tags[L3_SETS][L3_WAYS];
+     unsigned long long int timeStamp[L3_SETS][L3_WAYS];
+     unsigned long long hitCount, missCount;
+   
+   void reset()
+   {
+    //clearing L3 cache
+    for(int i=0;i<L3_SETS;i++)
+    {
+    for(int j=0;j<L3_WAYS;j++)
+    {
+      tags[i][j]=0;
+    }
+   }
+   //clearing L3 Timestamp
+   for(int i=0;i<L3_SETS;i++)
+   {
+    for(int j=0;j<L3_WAYS;j++)
+    {
+      timeStamp[i][j]=0;
+    }
+   }
+  }
+void print()
+{
+printf("--------------L3_DATA-----------\n");
+   for(int i=0;i<L3_SETS;i++)
+   {
+      for(int j=0;j<L3_WAYS;j++)
+      {
+         printf("%lld\t|",tags[i][j]);
+      
+      }
+      printf("\n");
+   }
+     printf("--------------L3_Timestamp-----------\n");
+   for(int i=0;i<L3_SETS;i++)
+   {
+      for(int j=0;j<L3_WAYS;j++)
+      {
+         printf("%lld\t|",timeStamp[i][j]);
+      
+      }
+      printf("\n");
+   }
+}
+int find_victim(int set)
+{  
+  int i,min=timeStamp[set][0],way=0;
+  for(i=1;i<L3_WAYS;i++)
+  {
+    if(timeStamp[set][i]<=min) 
+    { 
+        min=timeStamp[set][i];
+        way=i; 
+    }   
+  }
+  
+  return way;
+}
+void update_timestamp(int set,int way,int opr, unsigned long long * timeStampCntr)
+{
+  (*timeStampCntr)++;
+  //opr 0 mean invalidate
+  if(opr==0) 
+    timeStamp[set][way] = 0; 
+  else
+    //update timestamp
+    timeStamp[set][way] = *timeStampCntr; 
+}
+int search(int set,int tag)
+{
+  for(int i=0;i<L3_WAYS;i++) 
+  {
+    if((tags[set][i]==tag) && (timeStamp[set][i]!=0))
+      return i;
+  }
+  return -1; 
+}
+
+void insert(int set,int tag, unsigned long long * timeStampCntr)
+{
+  //way return the position to be evict, then insert data at way and update LRU table
+  int way =find_victim(set);
+  tags[set][way]=tag;
+  update_timestamp(set, way, 1, timeStampCntr); 
+}
+
+};
--- main.cpp
+++ main.cpp
@@ -1,40 +1,146 @@
 #include <iostream>
+#include "LLC.h"
+#include "L2.h"
 #include<cmath>
 using namespace std;
 
 #define CORE_COUNT 4
 
-#define L2_SETS 2048
-#define L2_WAYS 8
-
-#define L3_SETS 2048
-#define L2_WAYS 8
+#define L2_ID 0
+#define L3_ID 1
 
 
 /*Fuction to extract tag and set from given addr */
 void extract_TagSet(int cacheID, unsigned long long addr, unsigned long long *tag, unsigned long long *set)
 {
-  //cacheID is L2
-  if (cacheID == 0)
-  {
-    *set = (addr & 0xFFC0) >> 6;
-    *tag = (addr & 0xFFFFFFFFFFFF0000) >> log2(L2_SETS);
-  }
-  //cacheID is L3
-  else if (cacheID == 1)
-  {
-    *set = (addr & 0x1FFC0) >> 6;
-    *tag = (addr & 0xFFFFFFFFFFFE0000) >> log2(L3_SETS);
-  }
+    int lShift = 0;
+        //cacheID is L2
+    if (cacheID == 0)
+    {
+        *set = ((addr>> 6) & (int)(pow(2,log2(L2_SETS)+1)-1)) ;
+        lShift = (log2(L2_SETS) + 6);
+        *tag = (addr) >> lShift;
+    }
+    //cacheID is L3
+    else if (cacheID == 1)
+    {
+        *set = ((addr>> 6) & (int)(pow(2,log2(L3_SETS)+1)-1)) ;
+        lShift = (log2(L3_SETS) + 6);
+        *tag = (addr) >> lShift;
+    }
 }
 
 
-int main(){
-    // L2 L2[4];
+int main(int argc, char** argv)
+    {
+    L2 L2[CORE_COUNT];
+    LLC LLC;    
+    FILE *fp;
+    char input_name[100];
+    unsigned long long addr, newAddr, blockAddr, way, tag,set;
+    unsigned long long timeStampCntr = 0;
+    int threadid;
+    sprintf(input_name, "%s", argv[1]);
+    fp = fopen(input_name, "r");
+    //Checking error during file read
+    if (fp == NULL)
+    {
+        printf("Error!");
+        exit(1);
+    }
+    while (!feof(fp))
+    {
+        fscanf(fp, "%d  %llx\n", &threadid, &addr);
+        addr>>= 6;
+        //Reading the file
+       
+          //extract bits according to L2
+          extract_TagSet(L2_ID, addr, &tag, &set);
+          //search in l2
+          way= L2[threadid].search(set,tag);                                   
+          //way return -1 if not present in L2
+          if(way!=-1)
+          {
+            //L2 Hit
+            L2[threadid].hitCount++;
+            //update lru L2 timestamp
+            L2[threadid].update_timestamp(set, way, 1, &timeStampCntr);
+          }
+          else
+          { 
+            //L2 Miss 
+            L2[threadid].missCount++;
+            //extract bits according to L3
+            extract_TagSet(L3_ID, addr, &tag, &set);
+            //search in L3
+            way= LLC.search(set,tag);
+            //way return -1 if not present in L2
+            if(way!=-1)
+            { 
+              //L3 Hit 
+              LLC.hitCount++;
+              //update LRU L3 
+              LLC.update_timestamp(set, way, 1, &timeStampCntr);
+              //find L2 address and extract tag,set
+              extract_TagSet(L2_ID, addr, &tag, &set);
+              //Query L2 LRU for way no., fill L2 cache and Update LRU L2	
+              L2[threadid].insert(set,tag, &timeStampCntr);        
+            }
+            else
+            {
+              //L3 Miss
+              LLC.missCount++;
+              //query LRU L3 to get way no.
+              way=LLC.find_victim(set);
+              //if valid data is not present in L3 at way 
+              if(LLC.timeStamp[set][way]==0)
+              { 
+                //put data in L3 and update L3
+                 LLC.tags[set][way] = tag;
+                 LLC.update_timestamp(set, way, 1, &timeStampCntr); 
+                //Extract Tag and set for L2 and insert in L2
+                extract_TagSet(L2_ID, addr, &tag, &set);
+                L2[threadid].insert(set, tag, &timeStampCntr);         
+              } 
+              else  
+              //if conflict happens in L3 at way
+              {
+                //Get the tag of conflicting block and invalidate evicted line from L2 
+                tag = LLC.tags[set][way];
+                int shift = log2(L3_SETS);
+                newAddr = tag<< shift ;
+                newAddr=(newAddr|set);
+                newAddr<<=6;
+                extract_TagSet(L2_ID, newAddr, &tag, &set);
+                //get way
+                for(int i=0;i<CORE_COUNT;i++)
+                {
+                way=L2[i].search(set,tag);
+                if(way!=-1)
+                {
+                  //invalidate way in L2---set timestamp 0
+                   L2[i].update_timestamp(set, way, 0, &timeStampCntr);
+                  
+                }
 
-    // for(int i=0; i<){
+                }
+                //extract tag acc. to L3 and insert in L3  
+                extract_TagSet(L3_ID, addr, &tag, &set);
+                LLC.insert(set, tag, &timeStampCntr);
+                
+                //extract tag acc. to L2 and insert in L2
+                extract_TagSet(L2_ID, addr, &tag, &set);
+               L2[threadid].insert(set, tag, &timeStampCntr);
+              }
+            }
+          
+          }
+      }
+      fclose(fp);
+    
+    printf("\n\nUsing INCLUSIVE POLICY");
+    cout<<" L3 Hit:"<<LLC.hitCount <<" L3 Miss:"<<LLC.missCount;
+    //printf("\nL2 Hit  >= %lld\nL2 Miss = %lld\nL3 Hit  = %lld\nL3 Miss = %lld",L2.hitCount,L2.missCount,LLC.hitCount,LLC.missCount);
+    //print();
+  }
 
-    // }    
-    cout << log2(L2_SETS)<<" "<<log2(L2_WAYS);
-
-}
